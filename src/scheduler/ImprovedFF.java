0cdf0d408ad7d0dca61ade12c3d668ece5d0536e
JY2D <64841173+JY2D@users.noreply.github.com>
2021-05-31 13:38:55 +1000

    Update ImprovedFF.java

--- src/scheduler/ImprovedFF.java
+++ src/scheduler/ImprovedFF.java
@@ -4,6 +4,8 @@ import data.Job;
 import data.Server;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class ImprovedFF implements ServerProvider {
 
@@ -20,19 +22,13 @@ public class ImprovedFF implements ServerProvider {
 
     @Override
     public String getServer(String serverType, ArrayList<Server> serverList) {
-        for ( int i=0; i< serverList.size(); i++) {
-	Server server = serverList.get(i);
-            if (server.getCoreCount() >= job.getCore() && server.getDisk()>= job.getDisk() && server.getMemory()>= job.getMemory()) {
-                return server.getType() + " " + server.getId();
+    	Server min = serverList.get(0);
+
+        for (int i = 0; i < serverList.size(); i++) {
+            if (min.getWaittime() > serverList.get(i).getWaittime()) {                           
+                min = serverList.get(i); 
             }
         }
-        for (int i=0; i< dsServerArrayList.size();i++) {
-        Server server = dsServerArrayList.get(i);
-            if (server.getCoreCount() >= job.getCore() && server.getDisk() >= job.getDisk() && server.getMemory() >= job.getMemory()) {
-                server.setId(0);
-                return server.getType() + " " + server.getId();
-            }
-        }
-        return null;
-    }
+        return min.getType()+" "+min.getId(); 	  
+    }   
 }
